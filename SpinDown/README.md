f253dad7ba4a08a392bdf32829bb1b6bc6c838be
Davide Modolo <davide.modolo98@gmail.com>
2024-01-28 22:21:48 +0100

    moved the calculator to its own repository

--- SpinDown/README.md
+++
@@ -1,5 +0,0 @@
-TODO:
-
-- [ ] Add a description of an item when hover
-- [ ] Add analytics just to see the traffic
-- [ ] Add search bar in the second functionality results
--- SpinDown/items.py
+++
@@ -1,9 +0,0 @@
-file = open("items.txt", "r")
-names = open("names.txt", "r")
-names = names.readlines()
-for i, line in enumerate(file):
-    name = line[6:-5]
-    id = line.split("_")[0]
-    id = id.split("/")[-1]
-    name = name[len(id)+1:]
-    print(f"{{id: {id}, name: \"{name}\", suggestion: \"{names[i][:-1]}\" }},")
