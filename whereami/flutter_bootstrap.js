4ff710edf40302fe1118318d96a88988ead13afe
Davide Modolo <davide.modolo98@gmail.com>
2025-04-05 09:40:32 +0200

    add day3

--- whereami/flutter_bootstrap.js
+++ whereami/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3130434341"
+    serviceWorkerVersion: "2578795244"
   }
 });
